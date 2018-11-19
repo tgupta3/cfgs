6f64367413120e360451d699300e5dd114359b27
Tushar <tgupta3@ncsu.edu>
2018-11-18 21:42:16 -0500

    Added force checkout

--- .bin/install.sh
+++ .bin/install.sh
@@ -10,5 +10,5 @@ if [ $? = 0 ]; then
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
-config checkout
+config checkout -f
 config config status.showUntrackedFiles no
