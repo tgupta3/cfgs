0f4216a9ba233125a5775a24bea5c070e42ff7f6
Tushar <tgupta3@ncsu.edu>
2018-11-19 22:09:37 -0500

    Added force checkout

--- .bin/install.sh
+++ .bin/install.sh
@@ -3,12 +3,12 @@ function config {
    /usr/bin/git --git-dir=$HOME/.cfgs/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
-config checkout
+config checkout -f
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout -f 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 config checkout -f
 config config status.showUntrackedFiles no
